8a205d61503f6e13c95a32f72e7e3ee70e58de6c
cathrine <cvandamme2@academic.ca>
2026-03-18 14:22:25 -0500

    Created Eslint file

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+
+module.exports = [
+  js.configs.reccomended,
+  {
+    lnaguageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
+  }
+];
